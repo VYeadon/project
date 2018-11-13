37111443ea7aef8180d1b86c3c1459bc21c306f5
VYeadon <v.yeadon@hotmail.co.uk>
2018-11-13 14:07:08 +0000

    Added User data map to json converter

---
+++ src/main/java/com/qa/main/Runner.java
@@ -0,0 +1,24 @@
+package com.qa.main;
+
+import com.qa.other.Service;
+
+import come.qa.objectToJson.ConvertObjectsToJSON;
+
+public class Runner {
+	
+	public static void main( String[] args )
+    {
+		Service runnerObj = new Service();
+		
+		runnerObj.setPerson("mical", "person");
+		runnerObj.setPerson("Rohean", "Otherperson");
+		
+		runnerObj.getPerson(1);
+		runnerObj.getPerson(2);
+		
+		ConvertObjectsToJSON JSONConverter = new ConvertObjectsToJSON();
+		
+		JSONConverter.objectToJSONConverter(runnerObj.getPersonAccountMap(1));
+	}
+
+}
